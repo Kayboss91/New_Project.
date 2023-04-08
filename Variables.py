0b83be574cc137895b9d92afff6dc5aabe0abcf1
Kayboss91 <115898787+Kayboss91@users.noreply.github.com>
2023-04-08 19:05:35 +0100

    New Lesson

--- Variables.py
+++ Variables.py
@@ -1,5 +1,6 @@
 Person_name = "Kay"
-Person_age = 31
+Person_age = 35
+
 
 print("There was once a man named " + Person_name + ",")
 print("he was", Person_age, "years old")
@@ -12,3 +13,7 @@ print("but did not like being ", Person_age)
 
 print("What is my name ? ")
 
+print("whatis your name ?")
+
+print("did this change")
+# This is just a test
